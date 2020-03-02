752629b370d45e49d8a66ee2b5a095a9c8df67a7
Nicolai Emig <nicolai.emig@amseegym.de>
2020-03-02 09:13:46 +0100

    Analoguhr fehlt Review

--- lektion-18-Sortieralgorithmen/src/commonMain/kotlin/Main.kt
+++ lektion-18-Sortieralgorithmen/src/commonMain/kotlin/Main.kt
@@ -25,8 +25,8 @@ suspend fun main() {
             view.width = 510
             view.height = 250
             ArrayController.arrayErzeugen(100)
-            ArrayController.wartezeit = 1
-            ArrayController.sortieralgorithmus = BubbleSortSchlau
+            ArrayController.wartezeit = 50
+            ArrayController.sortieralgorithmus = BubbleSort
             ArrayController.sortieren()
         }
 
