dc60fbdff1ab54708cb54d7b3af176af5fc06677
Nicolai Emig <nicolai.emig@amseegym.de>
2020-03-08 22:16:34 +0100

    fertig stoppuhren

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 buildscript {
-    val engineEmiVersion = "0.50-SNAPSHOT"
+    val engineEmiVersion = "0.50"
 
 
     repositories {
-        mavenLocal()
-        //  maven { url = uri("https://dl.bintray.com/emign/engineEmi") }
+        maven { url = uri("https://dl.bintray.com/emign/engineEmi") }
         maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
         mavenCentral()
