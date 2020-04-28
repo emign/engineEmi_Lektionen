41bc5cd43751f6370079ab781942208d7d37a2ec
Nicolai Emig <nicolai.emig@amseegym.de>
2020-04-28 16:33:22 +0200

    0.60 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
         mavenCentral()
-        mavenLocal()
+
     }
     dependencies {
         classpath("me.emig:engineEmiGradlePlugin:$engineEmiVersion")
